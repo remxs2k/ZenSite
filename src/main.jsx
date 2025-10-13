a906bd9957e949c9dca09659b66661fd2409b890
remxs2k <remosnotar@gmail.com>
2025-10-14 01:01:44 +0300

    fixed rezervari.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
-import Rezervari from "./Rezervari.jsx"; // pasul următor vom crea acest fișier
+import Rezervari from "./rezervari.jsx"; // pasul următor vom crea acest fișier
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
