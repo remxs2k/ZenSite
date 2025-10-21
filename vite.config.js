98ba19f5b38660c21c3eda3c96a63a6015cd861a
remxs2k <remosnotar@gmail.com>
2025-10-21 23:27:37 +0300

    mobile view fix

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({
+      // remove babel-plugin-react-compiler — it's for React 19 only
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [],
       },
     }),
   ],
-})
+});
